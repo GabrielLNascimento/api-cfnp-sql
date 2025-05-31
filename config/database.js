cdc7a88807b9cb9bedb9b1ee28073d7d1fffa5ab
Gabriel De Liz <gabriellizcfnpbnu@gmail.com>
2025-05-30 23:03:15 -0300

    feat: new ambient variable

--- config/database.js
+++ config/database.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 module.exports = {
     development: {
-        username: 'sa', // ou outro usuário
-        password: 'ga02042007',
-        database: 'NOME_DO_BANCO',
-        host: 'localhost',
-        dialect: 'mssql',
+        username: process.env.DB_USER, // ou outro usuário
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
         dialectOptions: {
             options: {
                 encrypt: false, // true se estiver usando Azure
